c1ac57e906b1df5ea21a7475ea23cfad1f01594b
lordfeck <63065906+lordfeck@users.noreply.github.com>
2024-12-24 12:14:59 +0000

    better error handling, move agent to 'our' scope

--- last.played.psgi
+++ last.played.psgi
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-use v5.20;
+use v5.16;
 use strict;
 use warnings;
 
@@ -8,13 +8,25 @@ use lib 'lib';
 use Last::Played;
 use Plack::Request;
 
-# Test missing API key
-#my $lfm = Last::Played->new();
-
 my $key = $ENV{LASTFM_API_KEY};
 my $user = $ENV{LASTFM_USER_ID};
 
+die "FATAL: Please set an env var of LASTFM_API_KEY to store your API key!" unless $key;
+
 my $lfm = Last::Played->new(api_key => $key);
+
+sub get_last_played {
+    my $last_played = $lfm->get_last_played(shift);
+    my $status = ($last_played =~ /error/) ? 500 : 200;
+    say $status;
+
+    return [
+        $status,
+        [ 'Content-Type' => 'application/json' ],
+        [ $last_played ]
+    ];
+
+}
 
 my $app = sub {
     my $env = shift;
@@ -22,24 +34,14 @@ my $app = sub {
     my $req = Plack::Request->new($env);
  
     if ($req->param('user')) {
-        # todo: handle error response from API, should not be a 200
-        return [
-            '200',
-            [ 'Content-Type' => 'application/json' ],
-            [ $lfm->get_last_played($req->param('user')) ]
-        ];
+        get_last_played($req->param('user'));
     } elsif ($user) {
-        # todo: handle error response from API, should not be a 200
-        return [
-            '200',
-            [ 'Content-Type' => 'application/json' ],
-            [ $lfm->get_last_played($user) ]
-        ];
+        get_last_played($user);
     } else {
         return [
             '500',
-            [ 'Content-Type' => 'text/plain' ],
-            [ "UserID is required." ]
+            [ 'Content-Type' => 'application/json' ],
+            [ "{ message: \"UserID is required.\", status: \"error\"}" ]
         ];
     }
     
--- lib/Last/Played.pm
+++ lib/Last/Played.pm
@@ -10,6 +10,7 @@ use LWP::UserAgent;
 use HTTP::Request;
 
 our $VERSION = '1.00';
+our $AGENT = LWP::UserAgent->new(agent => "thransoft.last.played/$VERSION");
 
 =head1 NAME
 Last::Played
@@ -40,32 +41,32 @@ sub new {
     croak "Last.FM API key required." if !$opts{api_key} or !($opts{api_key} =~ /^\w+$/);
     my %self = (
         api_key => $opts{api_key},
-        get_recent_tracks => "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&api_key=$opts{api_key}&nowplaying=\"true\"&format=json&limit=1",
-        agent => LWP::UserAgent->new
+        get_recent_tracks => "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&api_key=$opts{api_key}&nowplaying=true&format=json&limit=1"
     );
-    $self{agent}->agent("thransoft.last.played/$VERSION");
     return bless \%self, $class;
 }
 
 sub get_last_played {
     my ($self, $user) = @_;
-    croak "Username missing" if !$user or !($user =~ /^\w+$/);
+    return "{message: \"username required\", status: \"error\"}" if !$user or !($user =~ /^\w+$/);
 
     # build url string - append user name query param
     my $reqUrl = "$self->{get_recent_tracks}&user=$user";
 
     my $req = HTTP::Request->new(GET => $reqUrl);
-    my $res = $self->{agent}->request($req);
+    my $res = $AGENT->request($req);
 
     if ($res->is_success) {
         my $json = decode_json($res->content);
         
         # splice it down, we don't need the boilerplate
         my $recent = $json->{recenttracks}->{track}[0];
+        return "{message: \"missing recent track data\", status: \"error\"}" unless $recent;
         my %jsonHash = (artist => $recent->{artist}->{'#text'}, album => $recent->{album}->{'#text'}, url => $recent->{url}, name => $recent->{name}, image => $recent->{image});
         return encode_json(\%jsonHash);
-    }
-    else {
+    } elsif ($res->code eq 401 or $res->code eq 403) {
+        return "{message: \"could not authenticate with last.fm\", status: \"error\"}";
+    } else {
         return $res->content, "\n";
     }
 }
