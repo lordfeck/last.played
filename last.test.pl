991e248a6fefe619d1dcbe2fbbda41e2e288a726
lordfeck <63065906+lordfeck@users.noreply.github.com>
2024-12-27 17:54:52 +0000

    add tests for missing key and user ID

--- last.test.pl
+++ last.test.pl
@@ -8,14 +8,28 @@ use lib 'lib';
 use Last::Played;
 
 # Test missing API key
-#my $lfm = Last::Played->new();
+eval { my $lfm1 = Last::Played->new(); };
+
+if ($@ =~ /key required/) {
+    say "missing api key failed successfully";
+} else {
+    die "missing api key was not caught!";
+}
 
 my $key = $ENV{LASTFM_API_KEY};
 my $USER = $ENV{LASTFM_USER_ID};
 
 my $lfm = Last::Played->new(api_key => $key);
 
-#say $lfm->get_last_played();
+# Test missing user 
+my $nouser = $lfm->get_last_played();
+if ($nouser =~ /username required/) {
+    say "missing user id failed successfully";
+} else {
+    die "missing user ID was not caught!";
+}
+
+# test fetches
 say $lfm->get_last_played($USER);
 say $lfm->get_top_track_alltime($USER);
 say $lfm->get_top_track_year($USER);
